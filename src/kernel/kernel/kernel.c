a07ac3ffedb7a36a78ca428ff045ac6d3e402ddd
Luke Mammen <lmammen96@gmail.com>
2020-10-04 19:40:53 -0500

    Add c/c++ global static constructor support

--- src/kernel/kernel/kernel.c
+++ src/kernel/kernel/kernel.c
@@ -2,7 +2,13 @@
 
 #include <kernel/tty.h>
 
-void kernel_main(void) {
+__attribute__( ( constructor ) ) void init_term( void )
+	{
 	terminal_initialize();
+	printf( "Term initialized, printf is available at this point.\n" );
+	}
+
+void kernel_main(void) {
+	// terminal_initialize();
 	printf("Hello, kernel World!\n");
 }
