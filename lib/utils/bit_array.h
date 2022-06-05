35e5e2d98a72ac63785d8b3e466976b3e808e365
Luke Mammen <lmammen96@gmail.com>
2022-06-04 19:07:34 -0500

    More work on page frame allocator

--- lib/utils/bit_array.h
+++ lib/utils/bit_array.h
@@ -1,19 +1,21 @@
 /*********************************************************************
 *
 *   MODULE:
-*       types.h
+*       bit_array.h
 *
 *   DESCRIPTION:
-*       description
+*       Bit Array manipulation
 *
 *********************************************************************/
 
-#ifndef _KERNEL_TYPES_H
-#define _KERNEL_TYPES_H
+#ifndef _BIT_ARRAY_H
+#define _BIT_ARRAY_H
 
 /*--------------------------------------------------------------------
                                INCLUDES
 --------------------------------------------------------------------*/
+
+#include <limits.h>
 
 /*--------------------------------------------------------------------
                           LITERAL CONSTANTS
@@ -39,11 +41,18 @@ extern "C" {
                                 MACROS
 --------------------------------------------------------------------*/
 
-#define CNT_OF_ARRAY(x) (sizeof(x)/sizeof(*x))
+#define BA_WIDTH( ba )      ( sizeof( (ba)[0] ) * CHAR_BIT )
+#define BA_MASK( ba )       ( ( 1ULL << BA_WIDTH( ba ) ) - 1UL )
+#define BA_BYTE( ba, i )    ( ( i ) / BA_WIDTH( ba ) )
+#define BA_BIT( ba, i )     ( ( i ) % BA_WIDTH( ba ) )
 
-#define set_bit(x,y) ((x) |= (1ULL << (y)))
-#define clear_bit(x,y) ((x) &= ~(1ULL << (y)))
-#define is_bit_set(x,y) ((x) & (1ULL << (y)))
+#define BA_CREATE64(name, cnt) uint64_t name[ ( cnt / ( sizeof(uint64_t) * CHAR_BIT ) ) ]
+#define BA_CREATE32(name, cnt) uint32_t name[ ( cnt / ( sizeof(uint32_t) * CHAR_BIT ) ) ]
+#define BA_CREATE16(name, cnt) uint16_t name[ ( cnt / ( sizeof(uint16_t) * CHAR_BIT ) ) ]
+#define BA_CREATE8(name, cnt) uint8_t name[ ( cnt / ( sizeof(uint8_t) * CHAR_BIT ) ) ]
+
+#define BA_SET( ba, i ) ( ba[ BA_BYTE( ba, i ) ] |= ( 1 << BA_BIT( ba, i ) ) )
+#define BA_CLR( ba, i ) ( ba[ BA_BYTE( ba, i ) ] &= ~( 1 << BA_BIT( ba, i ) ) )
 
 /*--------------------------------------------------------------------
                               PROCEDURES
@@ -52,4 +61,4 @@ extern "C" {
 #ifdef __cplusplus
 }
 #endif
-#endif /* _KERNEL_TYPES_H */
+#endif /* _BIT_ARRAY_H */
--- src/kernel/memory/page_frame.c
+++ src/kernel/memory/page_frame.c
@@ -13,6 +13,7 @@
 --------------------------------------------------------------------*/
 
 #include <assert.h>
+#include <limits.h>
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
@@ -20,32 +21,52 @@
 
 #include <kernel/types.h>
 
+#include <bit_array.h>
+
 /*--------------------------------------------------------------------
                           LITERAL CONSTANTS
 --------------------------------------------------------------------*/
 
 // TODO: Move arch to header
-#define PAGE_SHIFT 12
+#define PAGE_SHIFT  12ULL
+#define PAGE_SIZE   (1ULL << PAGE_SHIFT)
+#define PAGE_MASK   (~(PAGE_SIZE - 1))
 
-#define MAX_PAGES ( ( UINTPTR_MAX + 1 ) >> PAGE_SHIFT )
+#define MAX_PAGES   ( ( UINTPTR_MAX + 1ULL ) >> PAGE_SHIFT )
 
-#define BIT_ARR_WIDTH   sizeof(uint32_t)
-
-#define BIT_ARR_MASK    ( ( 1 << BIT_ARR_WIDTH ) - 1 )
 
 /*--------------------------------------------------------------------
                                 TYPES
 --------------------------------------------------------------------*/
 
+enum page_frame_flags
+    {
+    // __PAGE_FRAME_FLAGS__FREE = 1,
+    __PAGE_FRAME_FLAGS__ALLOCATED = 1,
+    __PAGE_FRAME_FLAGS__RESERVED,
+
+    // PAGE_FRAME_FLAGS__FREE        = 1ULL << __PAGE_FRAME_FLAGS__FREE,
+    PAGE_FRAME_FLAGS__ALLOCATED   = 1ULL << __PAGE_FRAME_FLAGS__ALLOCATED,
+    PAGE_FRAME_FLAGS__RESERVED    = 1ULL << __PAGE_FRAME_FLAGS__RESERVED
+    };
+
+struct page_frame
+    {
+    enum page_frame_flags
+                        flags;
+    uintptr_t           p_addr;     /* Physical address     */
+    };
+
 /*--------------------------------------------------------------------
                                 MACROS
 --------------------------------------------------------------------*/
 
-#define page_idx( x ) ( ( x ) / BIT_ARR_WIDTH )
-#define page_bit( x ) ( ( x ) % BIT_ARR_WIDTH )
+#define frame_alloc( pg_num )   BA_SET( used_pages, pg_num )
+#define frame_free( pg_num )    BA_CLR( used_pages, pg_num )
 
-#define frame_alloc( x ) used_pages[ page_idx( x ) ] |= (uint32_t)page_bit( x )
-#define frame_free( x ) used_pages[ page_idx( x ) ] &= ~(uint32_t)page_bit( x )
+#define addr2frame( pg_addr )    ( ( pg_addr ) >> PAGE_SHIFT )
+#define frame2addr( pg_frame )   ( ( pg_frame ) << PAGE_SHIFT )
+// #define frame_cnt( start, end )  ( ( ( end ) - ( start ) ) >> PAGE_SHIFT )
 
 /*--------------------------------------------------------------------
                            MEMORY CONSTANTS
@@ -55,9 +76,13 @@
                               VARIABLES
 --------------------------------------------------------------------*/
 
-static uint32_t used_pages[ MAX_PAGES / BIT_ARR_WIDTH ];
+static BA_CREATE32( used_pages, MAX_PAGES );
+                                    /* Bit array of used pages      */
+static uint32_t         last_alloc_frame;
+                                    /* Last allocated frame         */
 
-static uint32_t last_alloc_frame;
+static struct page_frame * 
+                        page_frames;/* Array of page frames         */ 
 
 /*--------------------------------------------------------------------
                               PROCEDURES
@@ -75,14 +100,34 @@ static uint32_t last_alloc_frame;
 *********************************************************************/
 void page_frame_pwrp
     (
-    void
+    uint32_t            ram_sz,     /* Size of RAM in bytes         */
+    uintptr_t           ram_base    /* 4k aligned addr after end of */
+                                    /* kernel                       */
     )
     {
+    /*------------------------------------------------------
+    Validate input
+    ------------------------------------------------------*/
+    assert( (ram_sz & PAGE_MASK) == 0 );
+    assert( (ram_base & PAGE_MASK) == 0 );
+
     /*------------------------------------------------------
     Init module variables
     ------------------------------------------------------*/
     memset( used_pages, 0x0, sizeof( used_pages ) );
     last_alloc_frame = 0;
+
+    /*------------------------------------------------------
+    Allocate page frame structures
+    ------------------------------------------------------*/
+    page_frames = (struct page_frame *)ram_base;
+
+    /*------------------------------------------------------
+    Mark page_frames as used
+    NOTE: 
+        Page frame zero corresponds to address zero.
+    ------------------------------------------------------*/
+    page_frame_reserve( addr2frame(ram_base), ram_sz >> PAGE_SHIFT );
 
     } /* page_frame_pwrp() */
 
@@ -113,6 +158,8 @@ void page_frame_reserve
     for( int i = 0; i < cnt; i++ )
         {
         frame_alloc( page_frame + i );
+
+        page_frames[ i ].flags |= PAGE_FRAME_FLAGS__RESERVED;
         }
 
     } /* page_frame_reserve() */
@@ -129,7 +176,7 @@ void page_frame_reserve
 *   TODO: Return page frame number or physical address?
 *
 *********************************************************************/
-void * page_frame_alloc
+struct page_frame * page_frame_alloc
     (
     void
     )
@@ -137,34 +184,37 @@ void * page_frame_alloc
     /*------------------------------------------------------
     Local Variables
     ------------------------------------------------------*/
-    uint32_t page_frame;
+    uint32_t frame_num;
     uint32_t idx = 0;
     uint32_t bit = 0;
     uint32_t i, j;
+    struct page_frame * frame = NULL;
 
     /*------------------------------------------------------
     Get next free index and bit number
     ------------------------------------------------------*/
-    idx = page_idx( last_alloc_frame );
-    bit = page_bit( last_alloc_frame );
+    // idx = BA_BYTE( used_pages, last_alloc_frame );
+    // bit = BA_BIT( used_pages, last_alloc_frame );
     
-    bit += 1;
-    if( bit >= BIT_ARR_WIDTH )
-        {
-        bit = 0;
-        idx++;
-        }
+    // bit += 1;
+    // if( bit >= BA_WIDTH( used_pages ) )
+    //     {
+    //     bit = 0;
+    //     idx++;
+    //     }
 
     /*------------------------------------------------------
     Search for a free page, starting at last alloc'd page
     frame.
     ------------------------------------------------------*/
-    for( i = idx; i < page_idx( MAX_PAGES ); i++ )
+    for( i = idx; i < CNT_OF_ARRAY( used_pages ); i++ )
         {
         /*--------------------------------------------------
-        No free pages at this index
+        No free pages at this index. I.e. all bits are set.
         --------------------------------------------------*/
-        if( used_pages[ i ] & BIT_ARR_MASK )
+        // TODO: Consider using cleared bit to represent used
+        // page frame. Then we could check for zero here.
+        if( used_pages[ i ] == BA_MASK( used_pages ) )
             {
             continue;
             }
@@ -172,7 +222,7 @@ void * page_frame_alloc
         /*--------------------------------------------------
         Search each bit for a free page
         --------------------------------------------------*/
-        for( j = bit; j < BIT_ARR_WIDTH; j++ )
+        for( j = bit; j < BA_WIDTH( used_pages ); j++ )
             {
             if( !( used_pages[ i ] & ( 1 << j ) ) )
                 {
@@ -183,33 +233,32 @@ void * page_frame_alloc
         /*--------------------------------------------------
         A free page was found
         --------------------------------------------------*/
-        if( BIT_ARR_WIDTH != j )
+        if( BA_WIDTH( used_pages ) != j )
             {
             break;
             }
-
         }
 
     /*------------------------------------------------------
     Out of pages
     ------------------------------------------------------*/
-    if( ( i >= page_idx( MAX_PAGES ) )
-     || ( j >= BIT_ARR_WIDTH         ) )
+    if( ( i >= CNT_OF_ARRAY( used_pages ) )
+     || ( j >= BA_WIDTH( used_pages )     ) )
         {
         assert( false );
         }
 
     /*------------------------------------------------------
-    Get page frame number
+    Get page frame
     ------------------------------------------------------*/
-    idx = i;
-    bit = j;
-    page_frame = ( idx * BIT_ARR_WIDTH ) + bit;
+    frame = &page_frames[ ( i * BA_WIDTH( used_pages ) ) + j ];
+
+    frame->flags |= PAGE_FRAME_FLAGS__ALLOCATED;
 
     /*------------------------------------------------------
-    Return physical address of allocated page frame
+    Return page frame
     ------------------------------------------------------*/
-    return page_frame * ( 1 << PAGE_SHIFT );
+    return frame;
 
     } /* page_frame_alloc() */
 
